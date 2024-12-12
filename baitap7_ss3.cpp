b9accfc58cd1da9dc56d696e7ff561552b99d116
PhatNguyen06 <nguyendaiphat06@gmail.com>
2024-12-12 10:26:06 +0700

    Update baitap7_ss3.cpp

--- baitap7_ss3.cpp
+++ baitap7_ss3.cpp
@@ -6,18 +6,21 @@ int main()
 	printf("Nhap n: ");
 	scanf("%d", &n);
 	
-	while(n <= 999 || n >= 10000)
+	if(n <= 999 || n >= 10000)
 	{
 		printf("Yeu cau nhap n co 4 chu so. Nhap lai: ");
 		scanf("%d", &n);
 	}
 	
 	int sum = 0;
-	for(int i = 0; i < 4; i++)
-	{
-		sum += n % 10;
-		n = n / 10;
-	}
+	sum += n % 10;
+	n = n / 10;
+	sum += n % 10;
+	n = n / 10;
+	sum += n % 10;
+	n = n / 10;
+	sum += n % 10;
+	n = n / 10;
 	
 	printf("Tong chu so da nhap la: %d", sum);
 	
