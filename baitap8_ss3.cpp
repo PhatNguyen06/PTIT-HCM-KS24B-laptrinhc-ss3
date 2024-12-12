e031e8826362a4e607c8c4595722673e9c8cb6f2
PhatNguyen06 <nguyendaiphat06@gmail.com>
2024-12-12 10:26:26 +0700

    Update baitap8_ss3.cpp

--- baitap8_ss3.cpp
+++ baitap8_ss3.cpp
@@ -6,18 +6,21 @@ int main()
 	printf("Nhap n: ");
 	scanf("%d", &n);
 	
-	while(n <= 999 || n >= 10000)
+	if(n <= 999 || n >= 10000)
 	{
 		printf("Yeu cau nhap n co 4 chu so. Nhap lai: ");
 		scanf("%d", &n);
 	}
 	
 	int so_nghich_dao = 0;
-	for(int i = 0; i < 4; i++)
-	{
-		so_nghich_dao = so_nghich_dao * 10 + (n % 10);
-		n = n / 10;
-	}
+	so_nghich_dao = so_nghich_dao * 10 + (n % 10);
+	n = n / 10;
+	so_nghich_dao = so_nghich_dao * 10 + (n % 10);
+	n = n / 10;
+	so_nghich_dao = so_nghich_dao * 10 + (n % 10);
+	n = n / 10;
+	so_nghich_dao = so_nghich_dao * 10 + (n % 10);
+	n = n / 10;
 	
 	printf("So sau khi duoc nghich dao = %d", so_nghich_dao);
 	
